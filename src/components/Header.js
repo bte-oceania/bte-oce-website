4bf963e36d0c7840461b2d7d30fdc476f4379420
Tom Varga <tomvarga@proton.lan>
2020-09-15 21:14:35 +1000

    got landing page done

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return(
         <div className="header">
             <ul className="links">
-                    <li><Link to="/"><h2 id="title">Build the Earth | NSW and ACT</h2></Link></li>
+                    <li><Link to="/"><h2 id="title">Build the Earth | Oceania</h2></Link></li>
                     <li><Link to="/build-zones" id="nav_button">Our Builds</Link></li>
                     <li><Link to="/the-team" id="nav_button">The Team</Link></li>
                     <li><Link to="/"><img id="logo" alt="logo" src="https://cdn.discordapp.com/icons/740864442836713483/52e9b39698b23c05bc352d9ccf892948.webp?size=128" /></Link> </li>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import '../css/Home.css'
 
-const Home = () => {
-    return(
-        <div className="home_main">
-            home
-        </div>
-    );
+class Home extends React.Component{
+    render(){
+        return(
+            <div className="home_main">
+                <div className="landing_page">
+                    <div className="title-text" >
+                        <h1 id="top_title">OCEANIA</h1>
+                        <hr id="title_divider"/>
+                        <h3>Minecraft, 1:1 Scale</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Home; 

b0329e090b683a9f765cd24c9e08f218ea24fa93
Coppertine <37494321+Coppertine@users.noreply.github.com>
2020-10-04 17:44:47 +1000

    Made navbar sticky

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,13 +13,12 @@ const Header = () => {
                     <li><Link to="/the-team" className="nav_button">The Team</Link></li>
                     <li><Link to="#" className="nav_button">Apply</Link></li>
                     <li><Link to="#" className="nav_button">FAQ</Link></li>
-
-                    <li><Link to="/"><img id="logo" alt="logo" src="https://cdn.discordapp.com/icons/740864442836713483/a_8cd7460d93bfc2b3692c50868feda192.webp?size=128" /></Link> </li>
-                    
                 </ul>
             </nav>
         </header>
     );
 }
+
+
 
 export default Header;

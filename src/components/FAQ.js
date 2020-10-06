93e1543b7350642bd99356e3f20e30721ca670c0
Tom Varga <tomvarga@proton.lan>
2020-10-07 09:04:12 +1100

    quck fixes

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 // Very basic. Might require restructure into class-based component
-
 const FAQ = () => {
     return <div>FAQ</div>;
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
                     <li><Link to="/"><h2 className="title">Oceania Build Team</h2></Link></li>
                     <li><Link to="/showcase" className="nav_button">Showcase</Link></li>
                     <li><Link to="/the-team" className="nav_button">The Team</Link></li>
-                    <li><Link to="#" className="nav_button">Apply</Link></li>
-                    <li><Link to="#" className="nav_button">FAQ</Link></li>
+                    <li><Link to="/apply" className="nav_button">Apply</Link></li>
+                    <li><Link to="/faq" className="nav_button">FAQ</Link></li>
                 </ul>
             </nav>
         </header>

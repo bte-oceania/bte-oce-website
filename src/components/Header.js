1082cae3e45ab89770ae63a88ea18e8c88ff1ac2
Coppertine <37494321+Coppertine@users.noreply.github.com>
2020-11-05 23:38:51 +1000

    Complete Redo pt 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,6 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return(
         <header>
-            <nav>
-                <ul className="links">
-                    <li><Link to="/"><img src="https://cdn.discordapp.com/attachments/741210134642163713/745520614546341948/unknown.png" className="logo"></img></Link></li>
-                    <li><Link to="/"><h2 className="title">Oceania Build Team</h2></Link></li>
-                    <li><Link to="/showcase" className="nav_button">Showcase</Link></li>
-                    <li><Link to="/the-team" className="nav_button">The Team</Link></li>
-                    <li><Link to="/apply" className="nav_button">Apply</Link></li>
-                    <li><Link to="/faq" className="nav_button">FAQ</Link></li>
-                </ul>
-            </nav>
         </header>
     );
 }
--- src/components/pages/Apply.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// Very basic. Might require restructure into class-based component
-
-const Apply = () => {
-    return <div>Apply</div>;
-}
-
-export default Apply;
